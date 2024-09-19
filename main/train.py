872da038ec0b6f5b7bd51a3547d8c272142ce9db
marshackVB <carter.marshall@gmail.com>
2024-09-19 12:24:24 -0500

    single node cluster and python module reference

--- main/train.py
+++ main/train.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from pyspark.sql import SparkSession
 
-from vscode_extension.main.custom_funcs import get_spark_dataframe, transform_data, get_pipeline
+from main.custom_funcs import get_spark_dataframe, transform_data, get_pipeline
 
 mlflow.set_tracking_uri("databricks")
 mlflow.set_experiment("/Shared/marshall_carter")
